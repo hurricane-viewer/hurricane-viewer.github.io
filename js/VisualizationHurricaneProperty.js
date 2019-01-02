e9781db75d1a26ab922b572f00cfd7bedef9553e
Hugo Castaneda <hugodecasta@orange.fr>
2019-01-02 19:28:44 +0100

    Update Hurrocane props to handle time range change

--- js/VisualizationHurricaneProperty.js
+++ js/VisualizationHurricaneProperty.js
@@ -2,44 +2,42 @@
 
 async function HurricaneProperty(svg) {
 
-  console.log('loading data...')
-  let data = await loadCsv('json/storms.csv')
+  let array = [1,2,3,4]
+  console.log(array)
 
-  console.log('croping...')
-  data = cropPeriod(data, '01/01/2000', '01/01/2020')
-
-  console.log('nesting...')
-  data = nestById(data)
-
-  let wantsToSee = 5
-  let every = Math.ceil(data.length / wantsToSee)
-  let fullData = []
-  for(let datIndex in data)
-    if(datIndex%every == 0)
-      fullData.push(data[datIndex])
-  data = fullData
-
+  let fullData = null
   let hurricaneMap = {}
-  for(let i in data) {
-    hurricaneMap[data[i].key] = i
+  let fromTime = '01/01/1100'
+  let toTime = '01/01/9000'
+
+  async function loadAllData(from='01/01/1100', to='01/01/9000') {
+
+    if(fullData == null)
+      fullData = (await loadCsv('json/storms.csv')).filter(rec=>rec.wind!='')
+    let data = cropPeriod(fullData, from, to)
+    data = nestById(data)
+
+    hurricaneMap = {}
+    for(let i in data) {
+      hurricaneMap[data[i].key] = i
+    }
+
+    data.forEach(hur => {
+      let beginTime = hur.values[0].timestamp/1000
+      hur.values.forEach(dat => dat.timeFromBegin = 
+        dat.timestamp/1000 - beginTime
+      )
+      hur.timeLength = parseInt(hur.values[hur.values.length-1].timeFromBegin)
+      hur.winds = hur.values.map(val => {
+        return {wind:parseInt(val.wind),time:val.timeFromBegin}
+      })
+      hur.winds = hur.winds.filter(val => !isNaN(val.wind))
+    })
+
+    return data
   }
 
-  console.log(hurricaneMap)
-
-  data.forEach(hur => {
-    let beginTime = hur.values[0].timestamp/1000
-    hur.values.forEach(dat => dat.timestamp = 
-      dat.timestamp/1000 - beginTime
-    )
-
-    hur.timeLength = parseInt(hur.values[hur.values.length-1].timestamp)
-    hur.winds = hur.values.map(val => {
-      return {wind:parseInt(val.wind),time:val.timestamp}
-    })
-    hur.winds = hur.winds.filter(val => !isNaN(val.wind))
-  })
-
-  console.log(data.length + ' Hurricanes shown')
+  let data = await loadAllData(fromTime, toTime)
 
   // --------------------------------------------------------------------
   // ------------------------------------------ POSITIONNING DATA -------
@@ -60,33 +58,27 @@ async function HurricaneProperty(svg) {
   // ----------------------------------------------------- SCALES -------
 
   let timeLengthScale = d3.scaleLinear()
-    .domain([0,d3.max(data.map(dat=>{return dat.timeLength}))])
     .range([0, innerWidth]);
 
   let yScale = d3.scaleLinear()
-    .domain([0,d3.max(data.map(dat=>{
-      return d3.max(dat.winds.map(val=>{return val.wind}))
-    }))])
     .range([innerHeight, 0]);
 
 
   // --------------------------------------------------------------------
   // ------------------------------------------------- AXIS/GRIDS -------
   let x_axis = d3.axisBottom()
-                 .scale(timeLengthScale)
 
   let y_axis = d3.axisLeft()
-                 .scale(yScale)
-
+    
+  innerG.append("g")
+    .attr('class','x_axis')
+    .attr('transform','translate(0,'+innerHeight+')')
 
   innerG.append("g")
-     .attr('transform','translate(0,'+innerHeight+')')
-     .call(x_axis)
-
-  innerG.append("g")
-     .call(y_axis);
+    .attr('class','y_axis')
 
   let displayG = innerG.append('g')
+
 
   // --------------------------------------------------------------------
   // ----------------------------------------------- DISPLAY DATA -------
@@ -98,8 +90,24 @@ async function HurricaneProperty(svg) {
   var colorScheme = d3.scaleOrdinal(d3.schemeCategory10)
 
   function displayData(data) {
-    console.log(data.length)
 
+    // --- scales
+    timeLengthScale
+      .domain([0,d3.max(data.map(dat=>{return dat.timeLength}))])
+
+    yScale
+      .domain([0,d3.max(data.map(dat=>{
+        return d3.max(dat.winds.map(val=>{return val.wind}))
+      }))])
+
+    // --- axis
+    x_axis.scale(timeLengthScale)
+    y_axis.scale(yScale)
+
+    d3.select('.x_axis').transition().call(x_axis)
+    d3.select('.y_axis').transition().call(y_axis)
+
+    // --- display
     let displays = displayG.selectAll('path').data(data)
 
     displays.enter()
@@ -118,6 +126,7 @@ async function HurricaneProperty(svg) {
   // --------------------------------------------------------------------
   // -------------------------------------------- EVENT HANDELING -------
 
+  // ----- SELECT
   EventEngine.registerTo(EventEngine.EVT.hurricaneSelected,function(hurId) {
     if(hurricaneMap.hasOwnProperty(hurId)) {
       displayData([data[hurricaneMap[hurId]]])
@@ -128,6 +137,24 @@ async function HurricaneProperty(svg) {
     displayData(data)
   })
 
+  // ----- TIME RANGE
+  EventEngine.registerTo(EventEngine.EVT.fromTimeChange,async function(newFromTime) {
+    fromTime = newFromTime
+    let data = await loadAllData(fromTime, toTime)
+    console.log(hurricaneMap)
+    console.log(data)
+    displayData(data)
+  })
+
+  EventEngine.registerTo(EventEngine.EVT.toTimeChange,async function(newToTime) {
+    console.log('toTime')
+    toTime = newToTime
+    let data = await loadAllData(fromTime, toTime)
+    displayData(data)
+  })
+
+
+  // ----- HOVER
   EventEngine.registerTo(EventEngine.EVT.hurricaneMouseEnter,function() {
     // TODO DISPLAY DATA
   })
